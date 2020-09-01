c9fce527944342c69559163da5805ef6b6bf8992
yuniargb <ygbachri@gmail.com>
2020-09-01 20:23:42 +0700

    menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -199,11 +199,11 @@
                         <i class="far fa-fw fa-chart-bar"></i>
                         <span>Laporan Pembayaran Kelas</span></a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" href="/laporanpembayaranangkatan">
                         <i class="far fa-fw fa-chart-bar"></i>
                         <span>Laporan Pembayaran Angkatan</span></a>
-                </li>
+                </li> -->
                 @endif
                 @if(auth()->user()->role == 3 || auth()->user()->role == 5 || auth()->user()->role == 4 ||
                 auth()->user()->role == 7)
